51d5d1c7fee22f0428565a0a44e3d5a51b436c99
Opilane TTHK <opilane@tthk.ee>
2026-04-28 14:10:39 +0300

    University

--- University/Controllers/StudentController.cs
+++ University/Controllers/StudentController.cs
@@ -145,6 +145,70 @@ namespace University.Controllers
                 EnrollmentDate = student.EnrollmentDate
             };
             return View(vm);
-        }  
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(StudentUpdateViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var student = new Models.Student
+                {
+                    Id = vm.Id,
+                    LastName = vm.LastName,
+                    FirstMidName = vm.FirstMidName,
+                    EnrollmentDate = vm.EnrollmentDate
+                };
+
+                var studentUpdate = student.Id;
+               
+                _context.Update(student);
+               
+                await _context.SaveChangesAsync();
+
+                //Kui andmed on uuendatud, siis suunab tagasi Update vaatesse, kus saab kohe uuesti andmeid uuendada.
+                //Hetkel suunab Indexi vaatesse peale uuendust
+                return RedirectToAction(nameof(Update), new { id = studentUpdate });
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+        // tehke Delete Get meetod koos vaatega
+       
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var student = await _context.Students
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (student == null) return NotFound();
+
+            var vm = new StudentIndexViewModel
+            {
+                Id = student.Id,
+                LastName = student.LastName,
+                FirstMidName = student.FirstMidName,
+                EnrollmentDate = student.EnrollmentDate
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student != null)
+            {
+                _context.Students.Remove(student);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
