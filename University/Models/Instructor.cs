36b80540b21e1dca8064d7900bcff596367f6668
Opilane TTHK <opilane@tthk.ee>
2026-05-14 14:56:09 +0300

    University

---
+++ University/Models/Instructor.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace University.Models
+{
+    public class Instructor
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseId { get; set; }
+
+        public string Title { get; set; }
+
+        public int Credits { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public Department Departments { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
+    }
+
+}
