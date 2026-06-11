1f6912ea24e99df1e5402604742fb4df375400cb
Martin Tarassov <epicgamesmega1@gmail.com>
2026-06-11 11:32:56 +0300

    Fix Course - remove IFormFile and redundant Departments property

--- University/Models/Course.cs
+++ University/Models/Course.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace University.Models
@@ -13,17 +13,9 @@ namespace University.Models
         public int Credits { get; set; }
         public int DepartmentId { get; set; }
         public Department Department { get; set; }
-        public List<IFormFile> Files { get; set; }
 
         public IEnumerable<FileToApi> FilesToApi { get; set; }
             = new List<FileToApi>();
-
-
-        public Department Departments
-        {
-            get => Department;
-            set => Department = value;
-        }
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
